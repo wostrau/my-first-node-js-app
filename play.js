c2cf36f886e4b493c94dc2bcd7865b27b1ef1acc
wostrau <a.ostrovtsov@gmail.com>
2024-05-19 23:52:01 +0200

    added play.js with basics refresh

--- play.js
+++ play.js
@@ -1,11 +1,70 @@
 const firstName = 'Alex';
 const age = 35;
 const hasHobby = true;
+const person = {
+  name: 'Alex',
+  age: 35,
+  greet() {
+    return `Hi, I am ${this.name}`;
+  },
+};
+const hobbies = ['Sports', 'Cooking', 'Hiking'];
 
-const summarizeUser = (userName, userAge, hasHobby) => {
-  return `Name is ${userName}, age is ${userAge}, user has hobby is ${hasHobby}`;
+setImmediate(() => {
+  const enhancedHobbies = hobbies.map((h) => `hobby >>> ${h}`);
+  console.log('enhanced hobbies >>>', enhancedHobbies);
+});
+
+const fetchData = (callback) => {
+  setTimeout(() => {
+    callback('Done!');
+  }, 1500);
 };
 
-const result = summarizeUser(firstName, age, hasHobby);
+const fetchPromisedData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done! (promised)');
+    }, 1500);
+  });
 
-console.log(result);
+  return promise;
+};
+
+setTimeout(() => {
+  console.log('Timer is done!');
+  fetchData((text) => console.log(text));
+}, 2000);
+
+setTimeout(() => {
+  console.log('Timer is done! (promised)');
+
+  fetchPromisedData()
+    .then((text) => {
+      console.log(text);
+      return fetchPromisedData();
+    })
+    .then((secondText) => {
+      console.log(secondText);
+    });
+}, 2000);
+
+const summarizeUser = (userName, userAge, hasHobby) =>
+  `Name is ${userName}, age is ${userAge}, user has hobby is ${hasHobby}`;
+const printName = ({ name }) => name;
+
+for (let hobby of hobbies) {
+  console.log('hobby >>>', hobby);
+}
+
+const result = summarizeUser(firstName, age, hasHobby);
+const greeting = person.greet();
+const toArray = (...args) => args;
+const newArray = toArray('Apple', 'Orange', 'Banana');
+const printedName = printName(person);
+hobbies.push('Reading');
+
+console.log('summarized user >>>', result);
+console.log('greeting person >>>', greeting);
+console.log('to array >>>', newArray);
+console.log('printed name >>>', printedName);
