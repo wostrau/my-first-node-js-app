5b1cfbb5bbda963f7d437cd45bb5abc05575314e
wostrau <a.ostrovtsov@gmail.com>
2024-07-10 12:49:58 +0200

    styles moved to separate file in public folder

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 // });
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
